d9a8b0190124184d8f76a8cc1c033c50d57e1cc0
shaktawat19 <harshshaktawat.tech@gmail.com>
2025-03-13 12:23:14 +0530

    long time

---
+++ Accessibility.js
@@ -0,0 +1,14 @@
+/*
+aria-describedby:
+Links an element to a description or additional information.
+Primarily used for providing context or instructions.
+The described content is typically static and not dynamic.
+
+
+aria-live:
+Indicates that content is dynamic and changes over time.
+Allows screen readers to announce real-time updates.
+The updates are announced based on the aria-live level (polite, assertive, or off).
+
+
+*/
--- MachineCoding/TypeAhead.jsx
+++ MachineCoding/TypeAhead.jsx
@@ -1,60 +1,60 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from 'react'
 
 const STATE = {
-  LOADING: "LOADING",
-  ERROR: "ERROR",
-  SUCCESS: "SUCCESS",
-};
+  LOADING: 'LOADING',
+  ERROR: 'ERROR',
+  SUCCESS: 'SUCCESS',
+}
 
 export default function TypeAhead() {
-  const [query, setQuery] = useState("");
-  const [result, setResults] = useState([]);
-  const [status, setStatus] = useState(STATE.LOADING);
-  const cache = useRef({});
-  console.log(cache);
+  const [query, setQuery] = useState('')
+  const [result, setResults] = useState([])
+  const [status, setStatus] = useState(STATE.LOADING)
+  const cache = useRef({})
+  console.log(cache)
   useEffect(() => {
-    const abortController = new AbortController();
-    const { signal } = abortController;
+    const abortController = new AbortController()
+    const { signal } = abortController
 
     const fetchData = async () => {
       try {
-        setStatus(STATE.LOADING);
+        setStatus(STATE.LOADING)
         if (cache.current[query]) {
-          console.log("retrieved from cache");
-          setResults(cache.current[query]);
-          setStatus(STATE.SUCCESS);
-          return;
+          console.log('retrieved from cache')
+          setResults(cache.current[query])
+          setStatus(STATE.SUCCESS)
+          return
         }
 
-        console.log("API Call");
+        console.log('API Call')
         const res = await fetch(
           `https://dummyjson.com/products/search?q=${query}&limit=10`,
           { signal }
-        );
-        const data = await res.json();
-        setStatus(STATE.SUCCESS);
-        cache.current[query] = data.products;
-        setResults(data.products);
+        )
+        const data = await res.json()
+        setStatus(STATE.SUCCESS)
+        cache.current[query] = data.products
+        setResults(data.products)
       } catch (error) {
-        console.log(error.name);
-        if (error.name !== "AbortError") {
-          setStatus(STATE.ERROR);
+        console.log(error.name)
+        if (error.name !== 'AbortError') {
+          setStatus(STATE.ERROR)
         }
       }
-    };
+    }
 
-    const timerId = setTimeout(fetchData, 1000);
+    const timerId = setTimeout(fetchData, 1000)
 
     return () => {
-      clearTimeout(timerId);
-      abortController.abort();
-    };
-  }, [query]);
+      clearTimeout(timerId)
+      abortController.abort()
+    }
+  }, [query])
 
   return (
     <div>
       <input
-        type="text"
+        type='text'
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
@@ -63,10 +63,106 @@ export default function TypeAhead() {
       {status === STATE.SUCCESS && (
         <ul>
           {result.map((product) => {
-            return <li key={product.id}>{product.title}</li>;
+            return <li key={product.id}>{product.title}</li>
           })}
         </ul>
       )}
     </div>
-  );
+  )
 }
+
+/*
+Improvements : 
+
+1. Accessibility Improvements:
+ =>
+  <label htmlFor="search-query">Search Products:</label>
+  <input
+  id="search-query"
+  type="text"
+  value={query}
+  onChange={(e) => setQuery(e.target.value)}
+  aria-describedby="search-query-description"
+  />
+  <p id="search-query-description">Search for products by name.</p>
+
+=> You can use aria-live to announce dynamic content changes like loading or errors.
+  <div aria-live="polite">
+    {status === STATE.ERROR && <p>Error found</p>}
+    {status === STATE.LOADING && <p>Loading data...</p>}
+  </div>
+
+2. Scalablity Improvements:
+=> debouncing instead of setTimeout
+=> Infinite loading, paginated loading (if necessary)
+
+3. Modularity Improvements:
+=> create custom hooks:
+  // useFetchProducts.js
+    import { useState, useEffect, useRef } from 'react';
+    const useFetchProducts = (query) => {
+      const [products, setProducts] = useState([]);
+      const [status, setStatus] = useState();
+      const cache = useRef({});
+
+      useEffect(() => {
+        const abortController = new AbortController();
+        const { signal } = abortController;
+
+        async function fetchData() {
+          try {
+            setStatus(STATE.LOADING);
+            if (cache.current[query]) {
+              setProducts(cache.current[query]);
+              setStatus(STATE.SUCCESS);
+              return;
+            }
+
+            const res = await fetch(
+              `https://dummyjson.com/products/search?q=${query}&limit=10`,
+              { signal }
+            );
+            const result = await res.json();
+            setProducts(result.products);
+            cache.current[query] = result.products;
+            setStatus(STATE.SUCCESS);
+          } catch (error) {
+            if (error.name !== 'AbortError') setStatus(STATE.ERROR);
+          }
+        }
+
+        let timer = setTimeout(() => fetchData(), 1000);
+
+        return () => {
+          clearTimeout(timer);
+          abortController.abort();
+        };
+      }, [query]);
+
+      return { products, status };
+    };
+
+    // App.js
+    const { products, status } = useFetchProducts(query);
+
+4. Performance Improvements:
+=> Error handling & response validation:
+    if (result && result.products) {
+      setProducts(result.products);
+      cache.current[query] = result.products;
+    } else {
+      setStatus(STATE.ERROR);
+    }
+=> avoid expensive re-renderings:
+  const productItems = useMemo(() => {
+    return products.map((product) => (
+      <li key={product.key}>{product.title}</li>
+    ));
+  }, [products]);
+
+  return <ul>{productItems}</ul>;
+
+5. Error boundaries
+
+
+*/
